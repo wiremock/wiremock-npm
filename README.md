620234e66004ca9e1fe244680eb1adc80262ce76
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-09-03 16:52:24 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wiremock NPM
+
+[![NPM](https://img.shields.io/npm/v/wiremock.svg?style=flat-square) ](https://www.npmjs.com/package/wiremock)
+
+This is [Wiremock Standalone](http://wiremock.org/docs/running-standalone/) wrapped inside an NPM package.
+
+Run it with:
+
+```shell
+npx wiremock
+```
+
+See: http://wiremock.org/docs/running-standalone/
---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo rm -rf jdeploy-bundle
+version=`awk 'NF>1{print $NF}' version.properties`
+echo "Version is: \"$version\""
+rm -rf wiremock*jar
+wget https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/$version/wiremock-jre8-standalone-$version.jar
+rm -rf package.json
+cp package.json.orig package.json
+sed -i "s/VERSION/$version/g" package.json
+sudo jdeploy publish
+git tag $version
+git push --tags
