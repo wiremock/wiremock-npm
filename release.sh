ced97b9fef5a53bb801f9ba7858630aa90ce1faf
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-09-06 21:10:15 +0200

    Without sudo jdeploy install

--- release.sh
+++ release.sh
@@ -3,10 +3,11 @@ sudo rm -rf jdeploy-bundle
 version=`awk 'NF>1{print $NF}' version.properties`
 echo "Version is: \"$version\""
 rm -rf wiremock*jar
-wget https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/$version/wiremock-standalone-$version.jar
+wget –q https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/$version/wiremock-standalone-$version.jar
 rm -rf package.json
 cp package.json.orig package.json
 sed -i "s/VERSION/$version/g" package.json
-sudo jdeploy publish \
+npm install -g jdeploy
+jdeploy publish \
  && git tag $version \
  && git push --tags
