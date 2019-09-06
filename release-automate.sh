041f078ee5b0d0a904a450755f179bb4ea14314a
Travis CI <travis@travis-ci.org>
2019-09-06 21:29:36 +0200

    work

--- release-automate.sh
+++ release-automate.sh
@@ -11,13 +11,12 @@ fi
 cd ..
 echo "version = $versionToRelease" > version.properties
 
-#git config user.email "travis@travis-ci.org"
-#git config user.name "Travis CI"
-#git checkout master
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
+git checkout master
 git commit -a -m "Bump version to $versionToRelease"
-#git remote add origin-token https://$GH_TOKEN@github.com/tomasbjerre/wiremock-npm.git
-#git remote -v
-#git push -u origin-token master
-git push
+git remote add origin-token https://$GH_TOKEN@github.com/tomasbjerre/wiremock-npm.git
+git remote -v
+git push -u origin-token master
 
 ./release.sh
