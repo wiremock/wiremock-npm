2804370c4015744136538dbf1130b53d47411370
Travis CI <travis@travis-ci.org>
2021-05-18 16:41:19 +0200

    exiting if jar not available #2

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ sudo rm -rf jdeploy-bundle
 version=`awk 'NF>1{print $NF}' version.properties`
 echo "Version is: \"$version\""
 rm -rf wiremock*jar
-wget –q https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/$version/wiremock-standalone-$version.jar
+wget –q https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/$version/wiremock-standalone-$version.jar || exit 1
 rm -rf package.json
 cp package.json.orig package.json
 sed -i "s/VERSION/$version/g" package.json
