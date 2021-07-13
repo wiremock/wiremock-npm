eabcadfe7dc813bb2b834ac4625717cdbd471765
Travis CI <travis@travis-ci.org>
2021-07-13 12:51:58 +0200

    Bump version to 2.29.0

--- release.sh
+++ release.sh
@@ -4,11 +4,16 @@ version=`awk 'NF>1{print $NF}' version.properties`
 echo "Version is: \"$version\""
 rm -rf wiremock*jar
 wget –q https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/$version/wiremock-jre8-standalone-$version.jar
-ls -hs wiremock-jre8-standalone-$version.jar && echo "file exists" || echo "does not exist" && exit 1
+ls -hs wiremock-jre8-standalone-$version.jar || (echo "does not exist" && exit 1)
+echo 1
 rm -rf package.json
+echo 12
 cp package.json.orig package.json
+echo 13
 sed -i "s/VERSION/$version/g" package.json
+echo 14
 npm install -g jdeploy
+echo 15
 jdeploy publish \
  && git tag $version \
  && git push -u origin --tags
